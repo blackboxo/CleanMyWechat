c857aa12082014060cf57191ea1859f3c3028246
blackboxo <592276898@qq.com>
2020-02-13 19:01:34 +0800

    first

---
+++ autodelete.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from autoui import *
+
+
+class MyWindow(QMainWindow, Ui_MainWin):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myWin = MyWindow()
+    myWin.show()
+    sys.exit(app.exec())
